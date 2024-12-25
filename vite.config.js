7f24b0d4947d2fe83be78dbf5a6883fc4ba394c6
Juan Leiton <ju.leiton@duocuc.cl>
2024-12-25 13:27:45 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/My_Web/', // Cambia "My_Web" por el nombre exacto de tu repositorio
+  base: '/My_Web/', // El nombre exacto del repositorio
 });
