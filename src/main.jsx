10f278e72ecaad8be18609db86df94ab4c2bf2ea
ItsJuanKamilo <ju.leiton@duocuc.cl>
2024-12-26 00:55:35 -0300

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom'; // Importa HashRouter
-import './index.css';
-import App from './App.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router> {/* Envuelve tu aplicación con HashRouter */}
-      <App />
-    </Router>
+    <App />
   </StrictMode>,
-);
+)
